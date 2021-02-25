dbdfec9ee084c953a9e0d31ce7fbaa20c16d22e8
ryanottobon <77082060+ryanottobon@users.noreply.github.com>
2021-02-25 17:38:59 -0300

    Add files via upload

---
+++ src/pack.js
@@ -0,0 +1,13 @@
+const pack = (prefix) => {
+return `╔════ ❯ 𝓹𝓪𝓬𝓴 𝓭𝓮 𝓱𝓮𝓷𝓽𝓪𝓲 𝓮 𝓸𝓷𝓵𝔂𝓯𝓪𝓷𝓼 𝓭𝓸 𝓶𝓮𝓰𝓪 ❮ ══════════
+║ ❯ *FOTOS DE HENTAI 720MB:* https://encurta.eu/YU7k 
+║ ❯ *PRINCESS STARLING(ONLY FANS)8.36GB:* https://encurta.eu/cBiTfDBd 
+║ ❯ *ELLES CLUB 28.68GB:* https://encurta.eu/ESTyP 
+║ ❯ *COCONUTKITTY(ONLY FANS)7.69GB:* https://encurta.eu/U2I5
+║ ❯ *Belle delphine:* https://encurta.eu/kgrp 
+║ ❯ *Kitty kum:* https://encurta.eu/lvh7YcTn 
+║ ❯ *Misaki Your Waifu:* https://encurta.eu/FObiTNG 
+║ ❯ *Love Lilah:* https://encurta.eu/Vbn3Sp 
+╚═══════════════════════════`
+}
+exports.pack = pack
---
+++ src/textcomands.js
@@ -0,0 +1,32 @@
+const textcomands = (p) => {
+return `𝕾𝖊𝖏𝖆𝖒 𝕭𝖊𝖒-𝖁𝖎𝖓𝖉𝖔𝖘 𝖆𝖔 𝕸𝖊𝖓𝖚 𝖉𝖊 𝕿𝖊𝖝𝖙𝖔 𝖉𝖔
+💎 ▁▂▃༻RY4N-BOT5༺▃▂▁ 💎
+╔════ ❯ 𝓬𝓶𝓭 𝓭𝓮 𝓽𝓮𝔁𝓽𝓸 ❮ ══════════
+║ ❯ *${p}woodboard* <text>
+║ ❯ *${p}undgrasstext* <text>
+║ ❯ *${p}overtext* <text>
+║ ❯ *${p}wdheartext* <text>
+║ ❯ *${p}whatis* <text>
+║ ❯ *${p}cstext* <text>
+║ ❯ *${p}warfacetext* <text>
+║ ❯ *${p}pubgtext* <t1> | <t2>
+║ ❯ *${p}8bits* <t1> | <t2>
+║ ❯ *${p}wolflogo* <t1> | <t2>
+║ ❯ *${p}ninjalogo* <t1> | <t2>
+║ ❯ *${p}lionlogo* <t1> | <t2>
+║ ❯ *${p}shadow* <text>
+║ ❯ *${p}glowmetal* <text>
+║ ❯ *${p}harrytext* <text>
+║ ❯ *${p}woodblock* <text>
+║ ❯ *${p}ocean* <text>
+║ ❯ *${p}coffe* <text>
+║ ❯ *${p}lovepaper* <text>
+║ ❯ *${p}glitch* <text>
+║ ❯ *${p}neonligth* <text>
+║ ❯ *${p}neontec* <text>
+║ ❯ *${p}snow* <text>
+║ ❯ *${p}blood* <text>
+║ ❯ *${p}dwater* <text>
+╚═══════════════════════════`
+}
+exports.textcomands = textcomands
